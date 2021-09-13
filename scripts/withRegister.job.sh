a83a3c9b1965b5aa7c912e185f59c51b6b4c159f
EthanLiupl <ethan.liu@presslogic.com>
2021-09-13 15:56:34 +0800

    feat: optimize

---
+++ scripts/withRegister.job.sh
@@ -0,0 +1,5 @@
+echo 'start running case without register...'
+echo 'finished running case without register...'
+
+#
+# curl -X 'POST' 'http://127.0.0.1:3000/uploadHtmlAndNoice'  -H 'accept: */*' -d ''
---
+++ scripts/withoutRegister.job.sh
@@ -0,0 +1,4 @@
+echo 'start running case with register...'
+echo 'finished running case with register...'
+#
+# curl -X 'POST' 'http://127.0.0.1:3000/uploadHtmlAndNoice'  -H 'accept: */*' -d ''
